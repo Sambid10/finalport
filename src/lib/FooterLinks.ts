66fb4495ad1011dbf6a75c6329c89621d9549c9c
Sambid <sambidshk10@gmail.com>
2025-12-03 11:33:32 +0545

    updated color

--- src/lib/FooterLinks.ts
+++ src/lib/FooterLinks.ts
@@ -2,21 +2,21 @@ export const footerLinks = [
   {
     title: "Facebook",
     href: "https://www.facebook.com/sambid.shakya",
-    color: "text-[#1DA1F2]" // blue
+    color: "text-blue-500" // close to #1DA1F2
   },
   {
     title: "LinkedIn",
     href: "https://www.linkedin.com/in/sambid-shakya-07546329b/",
-    color: "text-[#0A66C2]" // LinkedIn blue
+    color: "text-blue-700" // close to #0A66C2
   },
   {
     title: "Instagram",
     href: "https://www.instagram.com/sambidshakya/",
-    color: "text-[#E4405F]" // Instagram pink
+    color: "text-pink-500" // close to #E4405F
   },
   {
     title: "Github",
     href: "https://github.com/Sambid10",
-    color: "text-green-200" // GitHub green (or use black)
+    color: "text-gray-200" // instead of green-200
   }
 ];
